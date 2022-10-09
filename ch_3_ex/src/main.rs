2e7dbeffd9abd88375475ff36902373ca2911e70
Matt George <mdg.home@gmail.com>
2022-10-09 09:50:49 -0400

    Ch 3 Exercise 2

--- ch_3_ex/src/main.rs
+++ ch_3_ex/src/main.rs
@@ -1,10 +1,7 @@
+// Chapter 3 Exercises
+// ch03-05-control-flow.html
 
 use std::io;
-
-// Ex1: Convert temperatures between Fahrenheit and Celsius.
-// Ex2: Generate the nth Fibonacci number.
-// Ex3: Print the lyrics to the Christmas carol “The Twelve Days of Christmas,” 
-//      taking advantage of the repetition in the song.
 
 fn f_to_c(temp: i32) -> i32 {
     // (-40°F − 32) × 5/9 = -40°C
@@ -14,6 +11,8 @@ fn f_to_c(temp: i32) -> i32 {
 
 
 fn run_ex_1() {
+    // Ex1: Convert temperatures between Fahrenheit and Celsius.
+
     loop {
         let mut tempf: String = String::new();
         println!("Enter temp in Farenheit ('q' to exit):");
@@ -39,11 +38,46 @@ fn run_ex_1() {
 
 
 fn run_ex_2() {
-    println!("Unimplemented")
+    // Ex2: Generate the nth Fibonacci number.
+
+    loop {
+        println!("Enter which number in the Fibonacci sequence you would like ('q' to exit):");
+
+        let mut seq: String = String::new();
+        io::stdin()
+            .read_line(&mut seq)
+            .expect("Failed to read line");
+        
+        seq = seq.trim().to_string();
+        if seq == "q" {
+            break;
+        }
+
+        let seq: u32 = match seq.trim().parse() {
+            Ok(num) => num,
+            Err(_) => continue,
+        };
+
+        let mut i:u32 = 1;
+        let mut last:u32 = 0;
+        let mut fib:u32 = 1;
+
+        while i < seq {
+            let tmp: u32 = fib;
+            fib += last;
+            last = tmp;
+            i += 1;
+        }
+
+        println!("{fib}");
+    }
 }
 
 
 fn run_ex_3() {
+    // Ex3: Print the lyrics to the Christmas carol “The Twelve Days of Christmas,” 
+    //      taking advantage of the repetition in the song.
+
     println!("Unimplemented")
 }
 
