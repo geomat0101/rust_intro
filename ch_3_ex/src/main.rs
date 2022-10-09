68021e77aed22494ed9c79125e4d5d8e5976da87
Matt George <mdg.home@gmail.com>
2022-10-08 20:26:48 -0400

    ch3 exercise 1

--- ch_3_ex/src/main.rs
+++ ch_3_ex/src/main.rs
@@ -1,3 +1,33 @@
+
+use std::io;
+
+// Ex1: Convert temperatures between Fahrenheit and Celsius.
+// Ex2: Generate the nth Fibonacci number.
+// Ex3: Print the lyrics to the Christmas carol “The Twelve Days of Christmas,” 
+//      taking advantage of the repetition in the song.
+
+fn f_to_c(temp: i32) -> i32 {
+    // (-40°F − 32) × 5/9 = -40°C
+
+    (temp-32) * 5 / 9
+}
+
+
 fn main() {
-    println!("Hello, world!");
+    loop {
+        let mut tempf = String::new();
+        println!("Enter temp in Farenheit:");
+        io::stdin()
+            .read_line(&mut tempf)
+            .expect("Failed to read line");
+
+        let tempf: i32 = match tempf.trim().parse() {
+            Ok(num) => num,
+            Err(_) => continue,
+        };
+
+        let tempc: i32 = f_to_c(tempf);
+
+        println!("({tempf}°F − 32) × 5/9 = {tempc}°C");
+    }
 }
