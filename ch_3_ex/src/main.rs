2a60779d2569672540febcd08484cfa1ce1b4fd1
Matt George <mdg.home@gmail.com>
2022-10-09 09:02:52 -0400

    exercise abstraction, menuing

--- ch_3_ex/src/main.rs
+++ ch_3_ex/src/main.rs
@@ -13,13 +13,18 @@ fn f_to_c(temp: i32) -> i32 {
 }
 
 
-fn main() {
+fn run_ex_1() {
     loop {
         let mut tempf = String::new();
-        println!("Enter temp in Farenheit:");
+        println!("Enter temp in Farenheit ('q' to exit):");
         io::stdin()
             .read_line(&mut tempf)
             .expect("Failed to read line");
+        
+        tempf = tempf.trim().to_string();
+        if tempf == "q" {
+            break;
+        }
 
         let tempf: i32 = match tempf.trim().parse() {
             Ok(num) => num,
@@ -29,5 +34,45 @@ fn main() {
         let tempc: i32 = f_to_c(tempf);
 
         println!("({tempf}°F − 32) × 5/9 = {tempc}°C");
+    }
+}
+
+
+fn run_ex_2() {
+    println!("Unimplemented")
+}
+
+
+fn run_ex_3() {
+    println!("Unimplemented")
+}
+
+
+fn main() {
+    loop {
+        let mut choice = String::new();
+        
+        println!("
+Choose Exercise to Run ('q' to exit):
+    [1]: Convert temperatures between Fahrenheit and Celsius.
+    [2]: Generate the nth Fibonacci number.
+    [3]: Print the lyrics to the Christmas carol “The Twelve Days of Christmas,” 
+            taking advantage of the repetition in the song.
+Enter Choice:");
+
+        io::stdin()
+            .read_line(&mut choice)
+            .expect("Failed to read line");
+        
+        choice = choice.trim().to_string();
+        if choice == "q" {
+            break;
+        } else if choice == "1" {
+            run_ex_1();
+        } else if choice == "2" {
+            run_ex_2();
+        } else if choice == "3" {
+            run_ex_3();
+        }
     }
 }
